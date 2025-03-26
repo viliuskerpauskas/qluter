caf7711ffc3e9bb611316dd271a14ab697618c1b
viliuskerpauskas <126024911+viliuskerpauskas@users.noreply.github.com>
2025-03-26 16:34:08 +0200

    Add PrimeNG and themes to the project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
-
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
+import { primeNGCustomTheme } from './primeng-preset';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG(primeNGCustomTheme),
+  ],
 };
